e4a356844d1d8c64f7a37333c9870ee6c952f9b5
Joseph Griffin <93599297+joegriff19@users.noreply.github.com>
2022-11-17 11:31:59 +0100

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -20,54 +20,6 @@ means_df.columns = ['State', 'Age', 'Metal_Level', 'CSR', 'APTC',
                          'Med_Claims', 'Med_Spend', 'RX_Claims', 'MHP', 'Autopay']
 means_df = means_df.groupby('State').mean().reset_index()
 
-# # group different variables by state
-# metal_df = pcts_df.groupby(['State', 'Metal_Level']).size().unstack(fill_value=0).reset_index()
-# csr_df = pcts_df.groupby(['State', 'CSR']).size().unstack(fill_value=0).reset_index()
-# aptc_df = pcts_df.groupby(['State', 'APTC']).size().unstack(fill_value=0).reset_index()
-# mhp_df = pcts_df.groupby(['State', 'MHP']).size().unstack(fill_value=0).reset_index()
-# autopay_df = pcts_df.groupby(['State', 'Autopay']).size().unstack(fill_value=0).reset_index()
-#
-# # create new columns with percents
-# metal_df['All_bronze'] = metal_df['BRONZE'] + metal_df['EXPANDED BRO']
-# metal_df['Bronze_Pct'] = 100 * metal_df['All_bronze'] / (metal_df['All_bronze'] + metal_df['SILVER'] + metal_df['GOLD'])
-# metal_df['Silver_Pct'] = 100 * metal_df['SILVER'] / (metal_df['All_bronze'] + metal_df['SILVER'] + metal_df['GOLD'])
-# metal_df['Gold_Pct'] = 100 * metal_df['GOLD'] / (metal_df['All_bronze'] + metal_df['SILVER'] + metal_df['GOLD'])
-# csr_df['Yes_Pct'] = 100 * csr_df['Yes'] / (csr_df['Yes'] + csr_df['No'])
-# aptc_df['Yes_Pct'] = 100 * aptc_df['Yes'] / (aptc_df['Yes'] + aptc_df['No'])
-# mhp_df['Yes_Pct'] = 100 * mhp_df['Y'] / (mhp_df['Y'] + mhp_df['N'])
-# autopay_df['Yes_Pct'] = 100 * autopay_df['Y'] / (autopay_df['Y'] + autopay_df['N'])
-# autopay_df['Num_Members'] = autopay_df['Y'] + autopay_df['N']
-#
-# calculate grand total of all Ambetter members
-# member_total = means_df.Num_Members.sum()
-
-# # add row with averages to dfs in table
-# means_df.loc['<b>ALL<br>STATES</b>'] = means_df.mean()
-# csr_df.loc['<b>ALL<br>STATES</b>'] = csr_df.mean()
-# aptc_df.loc['<b>ALL<br>STATES</b>'] = aptc_df.mean()
-# mhp_df.loc['<b>ALL<br>STATES</b>'] = mhp_df.mean()
-# autopay_df.loc['<b>ALL<br>STATES</b>'] = autopay_df.mean()
-# metal_df.loc['<b>ALL<br>STATES</b>'] = metal_df.mean()
-#
-# # rename state as 'all states' in each df in table
-# means_df.State.loc['<b>ALL<br>STATES</b>'] = '<b>ALL<br>STATES</b>'
-# csr_df.State.loc['<b>ALL<br>STATES</b>'] = '<b>ALL<br>STATES</b>'
-# aptc_df.State.loc['<b>ALL<br>STATES</b>'] = '<b>ALL<br>STATES</b>'
-# mhp_df.State.loc['<b>ALL<br>STATES</b>'] = '<b>ALL<br>STATES</b>'
-# autopay_df.State.loc['<b>ALL<br>STATES</b>'] = '<b>ALL<br>STATES</b>'
-# metal_df.State.loc['<b>ALL<br>STATES</b>'] = '<b>ALL<br>STATES</b>'
-#
-# # replace mean total member number with actual total member number
-# autopay_df.Num_Members.loc['<b>ALL<br>STATES</b>'] = member_total
-
-# format numbers in table
-# means_df['Yes_Pct'] = means_df['Yes_Pct'].map('{:,.0f}%'.format)
-# means_df['Yes_Pct'] = means_df['Yes_Pct'].map('{:,.0f}%'.format)
-# means_df['Yes_Pct'] = means_df['Yes_Pct'].map('{:,.0f}%'.format)
-# means_df['Yes_Pct'] = means_df['Yes_Pct'].map('{:,.0f}%'.format)
-# means_df['Bronze_Pct'] = means_df['Bronze_Pct'].map('{:,.0f}%'.format)
-# means_df['Silver_Pct'] = means_df['Silver_Pct'].map('{:,.0f}%'.format)
-# means_df['Gold_Pct'] = means_df['Gold_Pct'].map('{:,.0f}%'.format)
 means_df['Age'] = means_df['Age'].map('{:.0f}'.format)
 means_df['Med_Claims'] = means_df['Med_Claims'].map('{:.0f}'.format)
 means_df['Med_Spend'] = means_df['Med_Spend'].map('${:,.0f}'.format)
