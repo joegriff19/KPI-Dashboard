5a9a4d39994fe6c25fbb1de93ba636c27c8bdb0f
Joseph Griffin <93599297+joegriff19@users.noreply.github.com>
2022-11-22 18:28:20 +0100

    Update page_5.py

--- pages/page_5.py
+++ pages/page_5.py
@@ -30,13 +30,7 @@ layout = html.Div(
                 dcc.Dropdown(
                     id='dropdown5',
                     multi=True,
-                    options=[
-                        {'label': "0", 'value': "0"},
-                        {'label': "1", 'value': "1"},
-                        {'label': "2", 'value': "2"},
-                        {'label': "3", 'value': "3"},
-                        {'label': "4", 'value': "4"},
-                    ],
+                    options=[{'label': x, 'value': x} for x in sep_reasons_22_df.Reason],
                     value=[0, 1, 2, 3, 4]
                     ),
                 dcc.Graph(
